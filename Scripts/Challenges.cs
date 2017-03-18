f57bc6ff21848fb570eedb75a7016293831d5642
Tanja Lamberg <tanja-96@hotmail.fi>
2017-03-18 22:29:36 +0200

    NYT DATABASE TOIMIIII!!!! <3

---
+++ Scripts/Challenges.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Challenges : MonoBehaviour {
+    public GameObject[] challenges = new GameObject[5];
+    public GameObject info;
+   
+
+	void Start () {
+        challenges = GameObject.FindGameObjectsWithTag("Challenge");
+        ChangeChallenges();
+    }
+	
+	void Update () {
+    }
+
+    public void SetPoints()
+    {
+        foreach(GameObject chal in challenges)
+        {
+            if (chal.GetComponent<Toggle>().isOn)
+            {
+                List<int> list = GameObject.Find("Jumala").GetComponent<DatabaseConnector>().GetPoints("SELECT Sos,Äly,Fyy,Hen,Tun FROM Tehtävät WHERE Title= " + chal.gameObject.GetComponentInChildren<Text>().text);
+                GameObject.Find("Jumala").GetComponent<DatabaseConnector>().UpdateDatabase("UPDATE Käyttäjä SET SosPisteet= SosPisteet +" + list[0] + ", ÄlyPisteet= ÄlyPisteet " + list[1] + ", FyyPisteet= FyyPisteet +" + list[2] + ", HenPisteet= HenPisteet +" + list[3] + ", TuPisteetn= TunPisteet +" + list[4] + " WHERE ID =1 ");
+            }
+        }
+    }
+
+    public void ChangeChallenges()
+    {
+        List<int> valitut = new List<int>();
+        //Tähän alkuun tulee kutsu Statseista, jonka avulla katsotaan pitääkö jotain osa-aluetta suosia.
+
+        //SOSIAALISUUS
+        List<int> IDt = GameObject.Find("Jumala").GetComponent<DatabaseConnector>().FindList("SELECT ID FROM Tehtävät WHERE Daily = 1 AND Sos > 0 AND Sos >= Fyy AND Sos >= Äly AND Sos >=Hen AND Sos >=Tun");
+        int rand = (int)Random.Range(0, IDt.Count);
+        int num = IDt[rand];
+        valitut.Add(num);
+        List<string> texts = GameObject.Find("Jumala").GetComponent<DatabaseConnector>().DataGetText("SELECT Title,Leipä FROM Tehtävät WHERE ID= " + num);
+        challenges[0].gameObject.GetComponentInChildren<Text>().text = texts[0];
+
+        //FYYSINEN
+        IDt = GameObject.Find("Jumala").GetComponent<DatabaseConnector>().FindList("SELECT ID FROM Tehtävät WHERE Daily = 1 AND Fyy > 0 AND Fyy >= Sos AND Fyy >= Äly AND Fyy >=Hen AND Fyy >=Tun");
+        for (int i = 0; i < IDt.Count; i++)
+        {
+            foreach (int x in valitut)
+            {
+                if (IDt[i] == x)
+                {
+                    IDt.Remove(x);
+                }
+            }
+        }
+        rand = (int)Random.Range(0, IDt.Count);
+        num = IDt[rand];
+        valitut.Add(num);
+        texts = GameObject.Find("Jumala").GetComponent<DatabaseConnector>().DataGetText("SELECT Title,Leipä FROM Tehtävät WHERE ID= " + num);
+        challenges[1].gameObject.GetComponentInChildren<Text>().text = texts[0];
+
+        //ÄLYLLINEN
+        IDt = GameObject.Find("Jumala").GetComponent<DatabaseConnector>().FindList("SELECT ID FROM Tehtävät WHERE Daily = 1 AND Äly > 0 AND Äly >= Sos AND Äly >= Fyy AND Äly >=Hen AND Äly >=Tun");
+        for (int i = 0; i < IDt.Count; i++)
+        {
+            foreach (int x in valitut)
+            {
+                if (IDt[i] == x)
+                {
+                    IDt.Remove(x);
+                }
+            }
+        }
+        rand = (int)Random.Range(0, IDt.Count);
+        num = IDt[rand];
+        valitut.Add(num);
+        texts = GameObject.Find("Jumala").GetComponent<DatabaseConnector>().DataGetText("SELECT Title,Leipä FROM Tehtävät WHERE ID= " + num);
+        challenges[2].gameObject.GetComponentInChildren<Text>().text = texts[0];
+
+        //HENKINEN
+        IDt = GameObject.Find("Jumala").GetComponent<DatabaseConnector>().FindList("SELECT ID FROM Tehtävät WHERE Daily = 1 AND Hen > 0 AND Hen >= Sos AND Hen >= Äly AND Hen >=Fyy AND Hen >=Tun");
+        for (int i = 0; i < IDt.Count; i++)
+        {
+            foreach (int x in valitut)
+            {
+                if (IDt[i] == x)
+                {
+                    IDt.Remove(x);
+                }
+            }
+        }
+        num = IDt[rand];
+        valitut.Add(num);
+        texts = GameObject.Find("Jumala").GetComponent<DatabaseConnector>().DataGetText("SELECT Title,Leipä FROM Tehtävät WHERE ID= " + num);
+        challenges[3].gameObject.GetComponentInChildren<Text>().text = texts[0];
+
+        //TUNTEELLINEN
+        IDt = GameObject.Find("Jumala").GetComponent<DatabaseConnector>().FindList("SELECT ID FROM Tehtävät WHERE Daily = 1 AND Tun > 0 AND Tun >= Sos AND Tun >= Äly AND Tun >=Hen AND Tun >=Fyy");
+        for (int i= 0; i < IDt.Count; i++)
+        {
+            foreach (int x in valitut)
+            {
+                if (IDt[i] == x)
+                {
+                    IDt.Remove(x);
+                }
+            }
+        }
+        rand = (int)Random.Range(0, IDt.Count);
+        num = IDt[rand];
+        valitut.Add(num);
+        texts = GameObject.Find("Jumala").GetComponent<DatabaseConnector>().DataGetText("SELECT Title,Leipä FROM Tehtävät WHERE ID= " + num);
+        challenges[4].gameObject.GetComponentInChildren<Text>().text = texts[0];
+        info.GetComponent<Text>().text = texts[1];
+    }
+
+}
---
+++ Scripts/DatabaseConnector.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mono.Data.Sqlite;
+using System.Data;
+using System;
+using System.IO;
+using UnityEngine.UI;
+
+
+public class DatabaseConnector : MonoBehaviour
+{
+    private float time=0;
+    private IDbConnection dbconn;
+    string conn = "";
+
+    void Awake()
+    {
+        string filepath = Application.persistentDataPath + "/" + "Data.db";
+        if (!File.Exists(filepath)) {
+            WWW loadDB = new WWW(streamingAssetsPath + "Data.db");
+            while (!loadDB.isDone) { }
+            File.WriteAllBytes(filepath, loadDB.bytes);
+        }
+        Debug.Log("Final PATH: " + filepath);
+        conn = "URI=file:" + filepath;
+        Debug.Log("Final PATH: " + conn);
+    }
+
+    public static string streamingAssetsPath
+    {
+        get
+        {
+            if (Application.platform == RuntimePlatform.Android)
+                return "jar:file://" + Application.dataPath + "!/assets/";
+            else
+                return "file://" + Application.streamingAssetsPath + "/";
+        }
+    }
+
+    void Start () {
+
+    }
+
+    void Update()
+    {
+
+    }
+	
+    public List<string> DataGetText(string x)
+    {
+        dbconn = (IDbConnection)new SqliteConnection(conn);
+        dbconn.Open(); //Open connection to the database.
+        List<string> lista = new List<string>();
+        IDbCommand dbcmd = dbconn.CreateCommand();
+        string sqlQuery = x;
+        dbcmd.CommandText = sqlQuery;
+        IDataReader reader = dbcmd.ExecuteReader();
+        while (reader.Read())
+        {
+            lista.Add(reader.GetString(0));
+            lista.Add(reader.GetString(1));
+        }
+        reader.Close();
+        reader = null;
+        dbcmd.Dispose();
+        dbcmd = null;
+        return lista;
+    }
+
+    public void UpdateDatabase(string x)
+    {
+        dbconn = (IDbConnection)new SqliteConnection(conn);
+        dbconn.Open(); //Open connection to the database.
+        IDbCommand dbcmd = dbconn.CreateCommand();
+        string sqlQuery = x;
+        dbcmd.CommandText = sqlQuery;
+        IDataReader reader = dbcmd.ExecuteReader();
+        reader.Close();
+        reader = null;
+        dbcmd.Dispose();
+        dbcmd = null;
+    }
+
+    public int getID(string x)
+    {
+        dbconn = (IDbConnection)new SqliteConnection(conn);
+        dbconn.Open(); //Open connection to the database.
+        IDbCommand dbcmd = dbconn.CreateCommand();
+        string sqlQuery = x;
+        dbcmd.CommandText = sqlQuery;
+        IDataReader reader = dbcmd.ExecuteReader();
+        int luku = 0;
+        while (reader.Read()) {
+            luku = reader.GetInt32(0);
+        }
+        reader.Close();
+        reader = null;
+        dbcmd.Dispose();
+        dbcmd = null;
+        return luku;
+    }
+
+    public List<int> FindList(string x)
+    {
+        dbconn = (IDbConnection)new SqliteConnection(conn);
+        dbconn.Open(); //Open connection to the database.
+        List<int> lista = new List<int>();
+        IDbCommand dbcmd = dbconn.CreateCommand();
+        string sqlQuery = x;
+        dbcmd.CommandText = sqlQuery;
+        IDataReader reader = dbcmd.ExecuteReader();
+        while (reader.Read())
+        {
+            lista.Add(reader.GetInt32(0));
+        }
+        reader.Close();
+        reader = null;
+        dbcmd.Dispose();
+        dbcmd = null;
+        return lista;
+    }
+
+    public List<int> GetPoints(string x)
+    {
+        dbconn = (IDbConnection)new SqliteConnection(conn);
+        dbconn.Open(); //Open connection to the database.
+        List<int> lista = new List<int>();
+        IDbCommand dbcmd = dbconn.CreateCommand();
+        string sqlQuery = x;
+        dbcmd.CommandText = sqlQuery;
+        IDataReader reader = dbcmd.ExecuteReader();
+        while (reader.Read())
+        {
+            lista.Add(reader.GetInt32(0));
+            lista.Add(reader.GetInt32(1));
+            lista.Add(reader.GetInt32(2));
+            lista.Add(reader.GetInt32(3));
+            lista.Add(reader.GetInt32(4));
+        }
+        reader.Close();
+        reader = null;
+        dbcmd.Dispose();
+        dbcmd = null;
+        return lista;
+    }
+}
